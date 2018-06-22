6c19311bcbb998b654b02cf7236b4e851eb9bd87
Ella Arana <ellamae207@gmail.com>
2018-06-22 13:33:27 -1000

    modified for cron

--- index.php
+++ index.php
@@ -9,13 +9,8 @@
 	ini_set( 'display_startup_errors', 1 );
 	error_reporting( E_ALL );
 	date_default_timezone_set("Pacific/Honolulu");
+	// $date_append = "20180621";
 	$date_append = date("Ymd");
-
-//When the list gets longer - manage partition number
-//***Cron how to determine what batch you are processing and how many batches
-//WP Data  --- circpro call only if no pubid from wp//
-//function below //
-//Refactor 1 task per function // 
 
 
 	$section_number = $_GET['section_number'];
@@ -23,46 +18,57 @@
 	
 	$time_start = microtime(true);
 
-	$url = "---"; //Daily Newspaper Alert 4,513
-//\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\
-//	1st STEP : CURL ITERABLE API 
-//	RESULT : CSV File from ITERABLE LIST
-//---------------------------------------------------------------------
-	$iterable_csv_filename = './temp/Iterable_List_' . $date_append . '.csv';
-	create_csv_from_url($url, $iterable_csv_filename); 
-	echo "Created Iterable CSV";
-//\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\
-//	2nd STEP : READ THE ITERABLE CSV , PARTITION AND FILTER ACTIVE USERS
-//	RESULT : ARRAY OF PENDING EMAILS
-//---------------------------------------------------------------------
-	filter_partitioned_emails($iterable_csv_filename, 3, $section_number);
-//\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\
-//	3rd STEP : Create CSV FOR PENDING EMAILS
-//---------------------------------------------------------------------
-	$pending_email_filename = './temp/'.$section_number.'_Pending_emails_' . $date_append . '.csv';
-	create_csv_from_array($pending_email_filename,$pending_emails);
-//\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\
-//	4th STEP : RECHECK PENDING EMAILS - Wordpress Data & Circpro Data (getCustomerAndBalance)
-//	RESULT : PROCESSED LIST - ACTIVE EMAILS & FAILED EMAILS
-//---------------------------------------------------------------------
-	$processed_email_filename = './unmerged_csv/'.$section_number.'_Processed_list_' . $date_append . '.csv';
-	$failed_email_filename = './temp/'.$section_number.'_Failed_list_' . $date_append . '.csv'; 
+	$url = "-"; //Daily Newspaper Alert 4,513
 
-	$row = 0;
-	if (($handle = fopen($pending_email_filename, "r")) !== FALSE) {
-		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-		    $num = count($data);
-		    $row++;
-		    for ($c=0; $c < $num; $c++) {	    	
-		    	$active = create_processed_list ($data[$c], $processed_email_filename, $failed_email_filename);
-	    	}
-		}
-	  fclose($handle);
+	$iterable_csv_filename		= './temp/Iterable_List_' . $date_append . '.csv';
+	$pending_email_filename 	= './temp/'.$section_number.'_Pending_emails_' . $date_append . '.csv';
+	$processed_email_filename 	= './unmerged_csv/'.$section_number.'_Processed_list_' . $date_append . '.csv';
+	$failed_email_filename 		= './temp/'.$section_number.'_Failed_list_' . $date_append . '.csv'; 
+	$divisor = 450 ; // number of emails you want per section 
+	$all_emails = array();
+	$section_emails;
+	$sections;
+	$processed_emails = array();
+	$pending_emails = array();	
+	$processed_emails_2 = array();
+	$failed_emails = array();
+	$final_email_list;
+
+//	If first run, then create the iterable CSV = $iterable_csv_filename
+	if ( $section_number == 0 ){
+		create_csv_from_url($url, $iterable_csv_filename); 
+		echo "<br>Created Iterable CSV for SECTION NUMBER : $section_number <br>";
 	}
+//	Break Iterable CSV into sections = $section_emails 
+	get_section_emails($iterable_csv_filename, $section_number, $divisor);
+	// print_pre($section_emails);
+//	Filter the section by WP = $processed_emails & $pending_emails
+	filter_active_emails_by_wp($section_emails);
+	// print_pre($processed_emails);
+	// print_pre($pending_emails);
+//	If there's $pending_emails , recheck by Circpro 
+//	If none, $processed_emails is the final list 
+	if(!empty($pending_emails)){
+		filter_active_emails_by_circpro($pending_emails);
+	}else{
+		create_csv_from_array($processed_email_filename, $processed_emails);
+	}
+//	Create CSV for reprocessed emails
+	if(!empty($processed_emails_2)){
+		global $final_email_list;
+		$final_email_list = array_merge($processed_emails, $processed_emails_2);
+		create_csv_from_array($processed_email_filename, $final_email_list);
+		create_csv_from_array($failed_email_filename, $failed_emails);
+	}else{
+		$final_email_list = $processed_emails;
+		create_csv_from_array($processed_email_filename, $final_email_list);
+		create_csv_from_array($failed_email_filename, $failed_emails);
+	}	
+
 
 	$time_end = microtime(true);
 	$time = ($time_end - $time_start)/60;
-	echo '<br>Execution time : '.$time.' minutes';
+	echo '<br>Execution time : '.$time.' minutes <br>';
 
 
 
@@ -78,9 +84,56 @@
 		// $outputcsv=file_get_contents($url, false, null, 0, 1000); // write few emails only characters for testing = 13 emails
 		file_put_contents($filename, $outputcsv);
 	}
+
+
+
+	function get_section_emails ($iterable_csv_filename, $section_number, $divisor ){
+		if (($handle = fopen($iterable_csv_filename, "r")) !== FALSE) {
+			global $section_emails;
+			global $all_emails;
+			echo "fopen";
+			$row = 0;
+			while (($data = fgetcsv($handle, 1000 , ",")) !== FALSE) {
+			    $num = count($data);
+			    $row++;
+			    for ($c=0; $c < $num; $c++) {	    	
+			    	$all_emails[] = $data[$c];
+
+		    	}
+			}
+		  fclose($handle);
+		}
+		// print_pre($all_emails);
+		$sections = array_chunk($all_emails, $divisor);
+		$section_emails =  $sections[$section_number];
+	}
+
+	function filter_active_emails_by_wp ($section_emails){
+		$total_section_emails = count($section_emails);
+		// print_pre($total_section_emails);		
+		for ($i=0; $i < $total_section_emails; $i++){
+			$email = $section_emails[$i];
+			if ( !empty($email) ) {
+				global $processed_emails;
+				global $pending_emails;
+				$subscriber = get_subscriber_wp_data($email);
+				$name_id = ($subscriber['name_id']);
+				$pub_id = ($subscriber['publication_id']);
+				if( $subscriber ){
+					$status = isCustomerAllowed($name_id, $pub_id);
+					if($status === true) {
+						$processed_emails[] = $email;
+					}else{
+						$pending_emails[] = $email;
+					}
+				}else{
+					$pending_emails[] = $email;
+				}
+			}
+		}
+	}
+
 	
-	$processed_emails = array();
-	$pending_emails = array();		
 	function filter_active_users ($email){
 		if ( !empty($email) ) {
 			global $processed_emails;
@@ -88,7 +141,7 @@
 			$subscriber = get_subscriber_wp_data($email);
 			$name_id = ($subscriber['name_id']);
 			$pub_id = ($subscriber['publication_id']);
-			if( $subscriber ){
+			if( $name_id && strlen($pub_id_wp) === 7 ){
 				$status = isCustomerAllowed($name_id, $pub_id);
 				if($status === true) {
 					$processed_emails[] = $email;
@@ -97,53 +150,14 @@
 				}
 			}else{
 				$pending_emails[] = $email;
+
 			}
 		}
 	}
 
-	function partition( $list, $p ) {
-	    $listlen = count( $list );
-	    $partlen = floor( $listlen / $p );
-	    $partrem = $listlen % $p;
-	    $partition = array();
-	    $mark = 0;
-	    for ($px = 0; $px < $p; $px++) {
-	        $incr = ($px < $partrem) ? $partlen + 1 : $partlen;
-	        $partition[$px] = array_slice( $list, $mark, $incr );
-	        $mark += $incr;
-	    }
-	    return $partition;
-	}
-
-	$all_emails = array();
-	$partitioned_emails;
-	function filter_partitioned_emails ($iterable_csv_filename, $partition_number, $section_number){
-		if (($handle = fopen($iterable_csv_filename, "r")) !== FALSE) {
-			global $partitioned_emails;
-			global $all_emails;
-			echo "fopen";
-			$row = 0;
-			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-			    $num = count($data);
-			    $row++;
-			    for ($c=0; $c < $num; $c++) {	    	
-			    	$all_emails[] = $data[$c];
-			    	$section = partition($all_emails, $partition_number);
-			    	$partitioned_emails =  $section[$section_number];
-			    	$emails_count = count($partitioned_emails);
-		    	}
-			}
-		  fclose($handle);
-		}
-		$totalSec = count($partitioned_emails);
-		for ($i=0; $i < $totalSec; $i++){
-			$active = filter_active_users($partitioned_emails[$i]);
-		}
-	}
 
 	function create_csv_from_array ($csv_name, $array) {
 		$file = fopen($csv_name,"w");
-
 		foreach ($array as $email)
 		{
 			fputcsv($file,explode(',',$email));
@@ -152,28 +166,16 @@
 	}
 
 
-
-
-	$processed_emails_2 = array();
-	$failed_emails = array();
-	$final_email_list;
-	function create_processed_list($email, $processed_email_filename, $failed_email_filename){
-		if ( !empty($email) ) {
-			global $processed_emails_2;
-			global $failed_emails;
-			global $processed_emails;
-			$subscriber = get_subscriber_wp_data($email);
-			$name_id = ($subscriber['name_id']);
-			$pub_id_wp = ($subscriber['publication_id']); 
-			$account_no_wp = ($subscriber['account_number']);
-			if( $name_id && strlen($pub_id_wp) === 7 )  {
-				$status = isCustomerAllowed($name_id, $pub_id_wp);
-				if($status === true){
-					$processed_emails_2[] = $email;
-				}else{
-					$failed_emails[] = $email;
-				}
-			}else if ( strlen($pub_id_wp) <= 7 ){
+	function filter_active_emails_by_circpro($pending_emails){
+		$total_pending_emails = count($pending_emails);
+		for ($i=0; $i < $total_pending_emails; $i++){
+			$email = $pending_emails[i];
+			if ( !empty($email) ) {
+				global $processed_emails;
+				global $pending_emails;
+				$subscriber = get_subscriber_wp_data($email);
+				$name_id = ($subscriber['name_id']);
+				$account_no_wp = ($subscriber['account_number']);
 				$get_pubID_with_getCustomerAndBalance = getCustomerAndBalance($name_id,$account_no_wp);
 				$pub_id_wp_2 = $get_pubID_with_getCustomerAndBalance['subscription']['publication_id'];
 				$status = isCustomerAllowed($name_id, $pub_id_wp_2);
@@ -182,28 +184,9 @@
 				}else{
 					$failed_emails[] = $email;
 				}				
-			}else{
-				$failed_emails[] = $email;
 			}
-		}else{
-			$failed_emails[] = $email;
 		}
-		global $date_append;
-
-
-		if(!empty($processed_emails_2)){
-			global $final_email_list;
-			$final_email_list = array_merge($processed_emails, $processed_emails_2);
-			create_csv_from_array($processed_email_filename, $final_email_list);
-			create_csv_from_array($failed_email_filename, $failed_emails);
-		}else{
-			$final_email_list = $processed_emails;
-			create_csv_from_array($processed_email_filename, $final_email_list);
-			// echo "<br>processed email csv created. <br>";
-			create_csv_from_array($failed_email_filename, $failed_emails);
-			// echo "<br>failed emails csv created. <br>";	
-		}
-		
 	}
-	print_pre($final_email_list);
+
+
 ?>
--- join.php
+++ join.php
@@ -9,34 +9,57 @@
 	error_reporting( E_ALL );
 	date_default_timezone_set("Pacific/Honolulu");
 	$date_append = date("Ymd");
+	// $date_append = "20180621";
 
 	function print_pre($object) {
 		?><pre><?php print_r($object); ?></pre><?php
 	}
 
 	$time_start = microtime(true);
+	$login = array(
+	    "ftp.ta.newsmemory.com" => array(
+	        "address" => "-",
+	        "user" => "-",
+	        "pwd" => '-',
+	        "dir" => "-"
+	    )
+	);
+	$url = "-"; //Pressreader Only
+	$pressreader_csv_filename = './unmerged_csv/Iterable_PressReader_List_' . $date_append . '.csv';
+	$list_1 = './unmerged_csv/0_Processed_list_' . $date_append . '.csv';
+	$list_2 = './unmerged_csv/1_Processed_list_' . $date_append . '.csv';
+	$list_3 = './unmerged_csv/2_Processed_list_' . $date_append . '.csv';
+	$list_4 = './unmerged_csv/3_Processed_list_' . $date_append . '.csv';
+	$list_5 = './unmerged_csv/4_Processed_list_' . $date_append . '.csv';
+	$list_6 = './unmerged_csv/5_Processed_list_' . $date_append . '.csv';
+	$list_7 = './unmerged_csv/6_Processed_list_' . $date_append . '.csv';
+	$list_8 = './unmerged_csv/7_Processed_list_' . $date_append . '.csv';
+	$list_9 = './unmerged_csv/8_Processed_list_' . $date_append . '.csv';
+	$list_10 = './unmerged_csv/9_Processed_list_' . $date_append . '.csv';
+	$list_11 = './unmerged_csv/10_Processed_list_' . $date_append . '.csv';
+	$list_12 = $pressreader_csv_filename;
+	$merged_list = './merged_csv/unfiltered_HSA_newspaperalerts_ACTIVE_' . $date_append . '.csv';
+	$final_list = './merged_csv/cron_HSA_newspaperalerts_ACTIVE_' . $date_append . '.csv';
+	$all_list = array($list_1, $list_2, $list_3, $list_4, $list_5, $list_6, $list_7, $list_8, $list_9, $list_10, $list_11, $list_12);
+
+//	create pressReader csv
+	create_csv_from_url($url, $pressreader_csv_filename);
+//	Joins all the files
+	joinFiles($all_list, $merged_list);
+//filter duplicate emails
+	filter_duplicate_emails($merged_list);
+//	Create final list 
+	create_csv_from_array($final_list, $cleansed_emails);
+//	Import the file to Tecnavia
+	ftpFile("ftp.ta.newsmemory.com", $final_list);
 
 
-	$url = "---"; //Pressreader Only
-
-	$iterable_csv_filename = './unmerged_csv/Iterable_PressReader_List_' . $date_append . '.csv';
-	create_csv_from_url($url, $iterable_csv_filename);
 
 	function create_csv_from_url ($url, $filename) {
 		$date_append = date("Ymd");
 		$outputcsv=file_get_contents($url);
 		file_put_contents($filename, $outputcsv);
 	}
-//\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\
-//	1st STEP : PATCH ALL PROCESSED FILES
-//	RESULT : FINAL CSV FILE for export to TENCNAVIA
-//---------------------------------------------------------------------
-	$list_1 = './unmerged_csv/0_Processed_list_' . $date_append . '.csv';
-	$list_2 = './unmerged_csv/1_Processed_list_' . $date_append . '.csv';
-	$list_3 = './unmerged_csv/2_Processed_list_' . $date_append . '.csv';
-	$list_4 = $iterable_csv_filename;
-	$final_list = './merged_csv/HSA_newspaperalerts_ACTIVE_' . $date_append . '.csv';
-
 
 	function joinFiles(array $files, $result) {
 		if(!is_array($files)) {
@@ -58,13 +81,8 @@
 		unset($wH);
 	}
 
-	$all_list = array($list_1, $list_2, $list_3, $list_4);
-	joinFiles($all_list, $final_list);
-	print_pre($final_list);
-
-
 	$cleansed_emails = array();
-	function filter_unique_emails ($csv_filename){
+	function filter_duplicate_emails ($csv_filename){
 		if (($handle = fopen($csv_filename, "r")) !== FALSE) {
 			global $cleansed_emails;
 			echo "fopen";
@@ -76,13 +94,111 @@
 			    for ($c=0; $c < $num; $c++) {	    	
 			    	$unfiltered_list[] = $data[$c];
 			    	$cleansed_emails = array_unique($unfiltered_list);
+			    	// print_pre($cleansed_emails);
 		    	}
 			}
 		  fclose($handle);
 		}
 	}
-	filter_unique_emails($final_list);
-	print_pre($cleansed_emails);
+
+
+
+	function create_csv_from_array ($csv_name, $array) {
+		$file = fopen($csv_name,"w");
+
+		foreach ($array as $email)
+		{
+			fputcsv($file,explode(',',$email));
+		}
+		fclose($file); 
+	}
+	
+
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// 	   Function:	ftpFile
+	//	Description:	Uploads a given file to the specified
+	//					FTP server. 
+	//		Created:	July 22, 2010
+	//		 Author:	Brant M. Songsong
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	function ftpFile($ftp_server,$local_file_path,$remote_path = null)
+	{
+		global $DEBUG, $login;
+
+		// Get username and password for this ftp server
+		$address = $login[$ftp_server]["address"];
+		$user = $login[$ftp_server]["user"];
+		$password = $login[$ftp_server]["pwd"];
+		$dir = isset($login[$ftp_server]["dir"]) ? $login[$ftp_server]["dir"] : null;
+
+		// Establish Connection to FTP Server
+		$conn = ftp_connect($address) or die("Couldn't connect to $address");
+
+		// Authenticate
+		if (ftp_login($conn,$user,$password))
+		{
+            if ($remote_path) {
+                $dir = $remote_path;
+            }
+
+            // If directory doesn't exist, create it
+            if (!@ftp_chdir($conn, $dir)) {
+                @ftp_mkdir($conn, $dir);
+                @ftp_chdir($conn, $dir);
+            }
+
+			// turn passive mode on
+			ftp_pasv($conn, true);
+
+            $local_file_path_array = array();
+			
+            // Convert to array if local_file_path is a string
+            if (!is_array($local_file_path)) { 
+                $local_file_path_array[] = $local_file_path;
+            }
+            else {
+                $local_file_path_array = $local_file_path;
+            }
+
+            foreach ($local_file_path_array as $local_file_path) {
+
+                // Open the file for reading (so pointer returns to start of file)
+                $feed_file_handle = fopen($local_file_path,"r");
+
+                // Define the Local File
+                $local_file = $local_file_path;
+
+                // Define the Remote File
+                $file_path = explode("/",$local_file_path);
+                $remote_file = $file_path[count($file_path)-1];
+
+                if ($DEBUG) { 
+                    echo "We're in!!!!<br /><br />"; 
+                }
+
+                // Copy the file to the FTP server
+                $file_move_success = ftp_put($conn,$remote_file,$local_file,FTP_BINARY);
+
+                if ($DEBUG)
+                {
+                    if ($file_move_success) {
+                        echo "Successfully uploaded $local_file_path" . "<br /><br />";
+                    } else {
+                        echo "There was a problem uploading $local_file_path" . "<br /><br />";
+                    } // end if
+                } // end if
+
+            } // end foreach
+
+
+		} // end if
+		
+		// Close FTP connection
+		ftp_close($conn); 
+		
+	} // end function
+	
 
 	$time_end = microtime(true);
 	$time = ($time_end - $time_start)/60;
